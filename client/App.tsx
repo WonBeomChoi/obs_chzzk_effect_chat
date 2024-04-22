8c1b6321fbb6fd1618ca44bee3eb4029a77d2591
jypark38 <jury124@naver.com>
2024-04-22 23:46:14 +0900

    App js 상수처리

--- client/App.tsx
+++ client/App.tsx
@@ -5,6 +5,12 @@ import ChatLayout from "./Components/ChatLayout";
 import { ChannelData } from "./types/chatProps.type";
 import EffectLayout from "./Components/EffectLayout";
 import { EffectType } from "./types/effect.type";
+import styled from "styled-components";
+
+const EFFECTS = [
+  { eventName: "차태경", effectName: "이지툰", runningTime: 2000 },
+  { eventName: "사출", effectName: "사출", runningTime: 3000 },
+];
 
 function App(props: ChannelData) {
   const [effect, setEffect] = useState<EffectType>({
@@ -12,31 +18,30 @@ function App(props: ChannelData) {
     effectName: "",
   });
   useEffect(() => {
-    window.addEventListener("차태경", () => {
-      if (effect) {
-        setEffect({ effect: false, effectName: "이지툰" });
-        setTimeout(() => {
-          setEffect({ effect: true, effectName: "" });
-        }, 2000);
-      }
-    });
-    window.addEventListener("사출", () => {
-      if (effect) {
-        setEffect({ effect: false, effectName: "사출" });
-        setTimeout(() => {
-          setEffect({ effect: true, effectName: "" });
-        }, 3000);
-      }
+    EFFECTS.forEach(({ eventName, effectName, runningTime }) => {
+      window.addEventListener(eventName, () => {
+        if (effect) {
+          setEffect({ effect: false, effectName: effectName });
+          setTimeout(() => {
+            setEffect({ effect: true, effectName: "" });
+          }, runningTime);
+        }
+      });
     });
   }, []);
   const chatList = useChat({ channelData: props });
 
   return (
-    <>
-      {/* <ChatLayout chatList={chatList} /> */}
-      <EffectLayout effect={effect.effect} effectName={effect.effectName} />;
-    </>
+    <Container>
+      <ChatLayout chatList={chatList} />
+      <EffectLayout effect={effect.effect} effectName={effect.effectName} />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  position: relative;
+  height: calc(100vh - 16px);
+`;
 
 export default App;
