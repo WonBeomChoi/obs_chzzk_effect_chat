0a52a67ce662f84a98ed4f07d73c0d37a39d0dc8
jypark38 <jury124@naver.com>
2024-04-29 23:58:18 +0900

    localStorage key 값 변경

--- project/client/utils/storage.ts
+++ project/client/utils/storage.ts
@@ -6,17 +6,17 @@ type GetConfigs = () => AppConfigs;
 // get localStorage
 // useEffect 내부에서 사용
 export const getConfig: GetConfigs = () => {
-  const config = localStorage.getItem('obs_chzzk');
+  const config = localStorage.getItem('obs_chzzk_config');
   try {
     if (!config) {
-      localStorage.setItem('obs_chzzk', JSON.stringify(DEFAULT_CONFIG));
+      localStorage.setItem('obs_chzzk_config', JSON.stringify(DEFAULT_CONFIG));
       return DEFAULT_CONFIG;
     }
     return JSON.parse(config);
   } catch (e) {
     // parse error
     console.error('error');
-    localStorage.setItem('obs_chzzk', JSON.stringify(DEFAULT_CONFIG));
+    localStorage.setItem('obs_chzzk_config', JSON.stringify(DEFAULT_CONFIG));
     return DEFAULT_CONFIG;
   }
 };
@@ -24,8 +24,8 @@ export const getConfig: GetConfigs = () => {
 // set localStorage
 // useEffect 내부에서 사용
 export const updateConfig = (data: any) => {
-  const prevConfig = localStorage.getItem('obs_chzzk');
+  const prevConfig = localStorage.getItem('obs_chzzk_config');
   const parsedPrevConfig = JSON.parse(prevConfig!);
 
-  localStorage.setItem('obs_chzzk', JSON.stringify({ ...parsedPrevConfig, ...data }));
+  localStorage.setItem('obs_chzzk_config', JSON.stringify({ ...parsedPrevConfig, ...data }));
 };
