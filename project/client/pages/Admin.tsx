ef3bfdd1ecf00d89ae2051e89f61009a2bf0ffcf
jypark38 <jury124@naver.com>
2024-05-03 14:28:04 +0900

    form 컴포넌트 분리

--- project/client/pages/Admin.tsx
+++ project/client/pages/Admin.tsx
@@ -1,4 +1,11 @@
-import React, { FormEventHandler, useEffect, useMemo, useRef, useState } from 'react';
+import React, {
+  Dispatch,
+  FormEventHandler,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import styled from 'styled-components';
 import { useConfigValues } from '../context/config';
 import { updateConfig } from '../utils/storage';
@@ -9,24 +16,70 @@ function Admin() {
     setStates: { effectInfos: setEffectInfos },
   } = useConfigValues();
 
+  const [editId, setEditId] = useState<string>('');
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     setIsLoading(false);
   }, []);
 
-  const idRef = useRef('');
-  const [eventName, setEventName] = useState('');
-  const [keyword, setKeyword] = useState('');
-  const [url, setUrl] = useState('');
-  const [runningTime, setRunningTime] = useState('');
-
   const EFFECTS = useMemo(() => Object.values(effectInfos), [effectInfos]);
 
+  const handleDelete = (id: string) => {
+    const newEffectInfos = { ...effectInfos };
+    delete newEffectInfos[id];
+    setEffectInfos(newEffectInfos);
+    updateConfig({
+      effectInfos: newEffectInfos,
+    });
+  };
+
+  const handleSetEditId = (id: string) => {
+    setEditId(id);
+  };
+
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
+
+  return (
+    <div style={{ display: 'flex', backgroundColor: 'white', height: '100vh' }}>
+      <TempForm editId={editId} setEditId={setEditId} />
+      <section>
+        {EFFECTS.map(({ id, effectUrl, eventName, keywords, runningTime }) => (
+          <div key={id} style={{ display: 'flex' }}>
+            <img src={effectUrl} alt="" style={{ width: '200px' }} />
+            <div>
+              <p>event name : {eventName}</p>
+              <p>keywords : {keywords.join(',')}</p>
+              <p>running time : {runningTime / 1000}초</p>
+              <button onClick={() => handleSetEditId(id)}>edit</button>
+              <button onClick={() => handleDelete(id)}>delete</button>
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+}
+
+const TempForm = (props: { editId: string; setEditId: Dispatch<SetStateAction<string>> }) => {
+  const {
+    states: { effectInfos },
+    setStates: { effectInfos: setEffectInfos },
+  } = useConfigValues();
+
+  const { editId, setEditId } = props;
+
+  const [eventName, setEventName] = useState('');
+  const [keywords, setKeyword] = useState('');
+  const [effectUrl, seteffectUrl] = useState('');
+  const [runningTime, setRunningTime] = useState('');
+
   const invalidateInput =
-    keyword.trim().length === 0 ||
+    keywords.trim().length === 0 ||
     eventName.trim().length === 0 ||
-    url.trim().length === 0 ||
+    effectUrl.trim().length === 0 ||
     runningTime.trim().length === 0 ||
     isNaN(Number(runningTime));
 
@@ -36,13 +89,13 @@ function Admin() {
       return;
     }
 
-    const id = idRef.current || String(Date.now());
+    const id = editId || String(Date.now());
 
     const newEffectInfo = {
       id,
       eventName,
-      keywords: keyword.split(','),
-      url,
+      keywords: keywords.split(','),
+      effectUrl,
       runningTime: Number(runningTime) * 1000,
     };
 
@@ -58,91 +111,51 @@ function Admin() {
 
     setEventName('');
     setKeyword('');
-    setUrl('');
+    seteffectUrl('');
     setRunningTime('');
-    idRef.current = '';
+    setEditId('');
   };
 
-  const handleDelete = (id: string) => {
-    const newEffectInfos = { ...effectInfos };
-    delete newEffectInfos[id];
-    setEffectInfos(newEffectInfos);
-    updateConfig({
-      effectInfos: newEffectInfos,
-    });
-  };
-
-  const handleEdit = (id: string) => {
-    const { eventName, keywords, url, runningTime } = effectInfos[id];
-    idRef.current = id;
-    setEventName(eventName);
-    setKeyword(keywords.join(','));
-    setUrl(url);
-    setRunningTime(String(runningTime / 1000));
-  };
-
-  if (isLoading) {
-    return <p>Loading</p>;
-  }
+  useEffect(() => {
+    if (editId.length !== 0) {
+      const { eventName, keywords, effectUrl, runningTime } = effectInfos[editId];
+      setEventName(eventName);
+      setKeyword(keywords.join(','));
+      seteffectUrl(effectUrl);
+      setRunningTime(String(runningTime / 1000));
+    }
+  }, [editId]);
 
   return (
-    <div style={{ backgroundColor: 'white', height: '100vh' }}>
-      <form action="" method="post" onSubmit={handleSubmit}>
-        <Input
-          name="eventName"
-          type="text"
-          placeholder="이벤트 이름"
-          value={eventName}
-          onChange={(e) => setEventName(e.target.value)}
-          autoComplete="off"
-        />
-        <Input
-          name="keyword"
-          type="text"
-          placeholder="키워드(쉼표로 구분)"
-          value={keyword}
-          onChange={(e) => setKeyword(e.target.value)}
-          autoComplete="off"
-        />
-        <Input
-          name="url"
-          type="text"
-          placeholder="이미지 주소"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          autoComplete="off"
-        />
-        <Input
-          name="url"
-          type="text"
-          placeholder="실행 시간 (초)"
-          value={runningTime}
-          onChange={(e) => setRunningTime(e.target.value)}
-          autoComplete="off"
-        />
-        <button type="submit" disabled={invalidateInput}>
-          제출
-        </button>
-      </form>
-      <section>
-        {EFFECTS.map((effect) => (
-          <div key={effect.id} style={{ display: 'flex' }}>
-            <img src={effect.url} alt="" style={{ width: '200px' }} />
-            <div>
-              <p>event name : {effect.eventName}</p>
-              <p>keywords : {effect.keywords.join(',')}</p>
-              <p>running time : {effect.runningTime / 1000}초</p>
-              <button onClick={() => handleEdit(effect.id)}>edit</button>
-              <button onClick={() => handleDelete(effect.id)}>delete</button>
-            </div>
-          </div>
-        ))}
-      </section>
-    </div>
+    <form style={{ width: '500px' }} onSubmit={handleSubmit}>
+      <Input
+        placeholder="이벤트 이름"
+        value={eventName}
+        onChange={(e) => setEventName(e.target.value)}
+      />
+      <Input
+        placeholder="키워드(쉼표로 구분)"
+        value={keywords}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
+      <Input
+        placeholder="이미지 주소"
+        value={effectUrl}
+        onChange={(e) => seteffectUrl(e.target.value)}
+      />
+      <Input
+        placeholder="실행 시간 (초)"
+        value={runningTime}
+        onChange={(e) => setRunningTime(e.target.value)}
+      />
+      <button type="submit" disabled={invalidateInput}>
+        제출
+      </button>
+    </form>
   );
-}
+};
 
-const Input = styled.input`
+const Input = styled.input.attrs({ type: 'text', autoComplete: 'off' })`
   display: block;
 `;
 
